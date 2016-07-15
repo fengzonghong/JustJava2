edd0cbdf370eac1f6e53b316026bf429f97ebafe
fengzonghogn <552006830@qq.com>
2016-07-15 11:59:52 +0800

    Initial commit just java2

---
+++ app/src/main/java/com/example/android/justjava2/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.android.justjava2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int numberOfCoffees=2;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * 5);
+
+    }
+    public void plusOrder(View view) {
+        quantity=quantity+1;
+        display(quantity);
+        displayPrice(quantity * 5);
+
+    }
+    public void minusOrder(View view) {
+        quantity=quantity-1;
+        display(quantity);
+        displayPrice(quantity * 5);
+
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
