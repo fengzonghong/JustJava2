0088b6daf6fc774c89103eaf2245ba6f13371865
fengzonghogn <552006830@qq.com>
2016-07-18 11:21:05 +0800

    price message.

--- app/src/main/java/com/example/android/justjava2/MainActivity.java
+++ app/src/main/java/com/example/android/justjava2/MainActivity.java
@@ -9,7 +9,7 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int quantity = 2;
+    public int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,22 +20,17 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view) {
-        int numberOfCoffees=2;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
 
-    }
-    public void plusOrder(View view) {
-        quantity=quantity+1;
-        display(quantity);
-        displayPrice(quantity * 5);
+   public void plusOrder(View view) {
+        quantity=quantity + 1;
+       displayQuantity(quantity);
+
+
 
     }
     public void minusOrder(View view) {
         quantity=quantity-1;
-        display(quantity);
-        displayPrice(quantity * 5);
+        displayQuantity(quantity);
 
     }
     /**
@@ -45,16 +40,41 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
     }
+    public void submitOrder(View view) {
+        int price=quantity*5;
+        String  priceMessage = createrOrderSummary(price);
 
+        displayMessage(priceMessage);
+        calculatePrice();
+
+    }
+    /**
+     * Calculates the price of the order.
+     *
+
+     */
+    private int calculatePrice() {
+        return quantity * 5;
+    }
+    private String createrOrderSummary(int price){
+        String priceMessage = "Name:Zonghong";
+        priceMessage = priceMessage + "\nQuantity" + quantity;
+        priceMessage = priceMessage + "\nIt's total $ " + price;
+        priceMessage = priceMessage + "\nThank you!";
+        displayMessage(priceMessage);
+        return priceMessage;
+
+    }
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    /**
+
+       /**
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
